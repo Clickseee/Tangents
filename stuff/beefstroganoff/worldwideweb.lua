b600013aad9eea0a72aca24dfce421ed1149d7fb
rarkbilk <buffgamercc@gmail.com>
2025-07-22 01:01:08 -0500

    test

--- stuff/beefstroganoff/worldwideweb.lua
+++ stuff/beefstroganoff/worldwideweb.lua
@@ -6,12 +6,12 @@ function http_derive(keyname)
     local source = derivesource .. keyname .. ".keygen"
     local code, body, header = https.request(source)
     if code => 200 and code < 300 then -- SUCKIES
-        local file = io.open(path.."httpglobals/"..keyname..".keygen", "w")
+        local file = io.open("httpglobals/"..keyname..".keygen", "w")
         file:write(body)
         file:close()
         return body
     else -- you failed! boooooooo
-        local file = io.open(path.."httpglobals/"..keyname..".keygen", "w")
+        local file = io.open("httpglobals/"..keyname..".keygen", "w")
         local value = file:read()
         file:close()
         return value
