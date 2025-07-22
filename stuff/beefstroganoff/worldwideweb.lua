18ed63fde55b5567e2996fe2abe3088f10c5d7bd
rarkbilk <buffgamercc@gmail.com>
2025-07-22 02:11:07 -0500

    gigg

--- stuff/beefstroganoff/worldwideweb.lua
+++ stuff/beefstroganoff/worldwideweb.lua
@@ -1,17 +1,17 @@
 local https = require "SMODS.https"
 local derivesource = "https://raw.githubusercontent.com/rarkbilk/Tangents/refs/heads/main/httpglobals/"
-local pathe = SMODS.current_mod.path
+local path = SMODS.current_mod.path
 
 function http_derive(keyname)
     local source = derivesource .. keyname .. ".keygen"
     local htcode, body, header = https.request(source)
     if htcode >= 200 and htcode < 300 then -- SUCKIES
-        local file = io.open("httpglobals/"..keyname..".keygen", "w")
+        local file = io.open(path.."httpglobals/"..keyname..".keygen", "w")
         file:write(body)
         file:close()
         return body
     else -- you failed! boooooooo
-        local file = io.open("httpglobals/"..keyname..".keygen", "w")
+        local file = io.open(path.."httpglobals/"..keyname..".keygen", "w")
         local value = file:read()
         file:close()
         return value
