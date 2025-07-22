df4d4383dff0dec9542ab0fcbf7edc8be0f11899
rarkbilk <buffgamercc@gmail.com>
2025-07-21 23:38:47 -0500

    tenna offline mode

--- stuff/beefstroganoff/worldwideweb.lua
+++ stuff/beefstroganoff/worldwideweb.lua
@@ -1,8 +1,19 @@
 local https = require "SMODS.https"
 local derivesource = "https://raw.githubusercontent.com/rarkbilk/Tangents/refs/heads/main/httpglobals/"
+local path = G.nxkoo_dies.path
 
 function http_derive(keyname)
     local source = derivesource .. keyname .. ".keygen"
     local code, body, header = https.request(source)
-    return body
+    if code => 200 and code < 300 then -- SUCKIES
+        local file = io.open(path.."httpglobals/"..keyname..".keygen", "w")
+        file:write(body)
+        file:close()
+        return body
+    else -- you failed! boooooooo
+        local file = io.open(path.."httpglobals/"..keyname..".keygen", "w")
+        local value = file:read()
+        file:close()
+        return value
+    end
 end
