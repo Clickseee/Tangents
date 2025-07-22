3f10da12baa07750e878792577df5987d1db932c
rarkbilk <buffgamercc@gmail.com>
2025-07-22 01:41:14 -0500

    d

--- stuff/beefstroganoff/worldwideweb.lua
+++ stuff/beefstroganoff/worldwideweb.lua
@@ -5,15 +5,15 @@ local pathe = SMODS.current_mod.path
 function http_derive(keyname)
     local source = derivesource .. keyname .. ".keygen"
     local code, body, header = https.request(source)
-    if code => 200 and code < 300 then -- SUCKIES
+    --if code => 200 and code < 300 then -- SUCKIES
         --[[local file = io.open("httpglobals/"..keyname..".keygen", "w")
         file:write(body)
         file:close()]]
         return body
-    else -- you failed! boooooooo
+    --else -- you failed! boooooooo
         --[[local file = io.open("httpglobals/"..keyname..".keygen", "w")
         local value = file:read()
         file:close()
         return value]]
-    end
+   -- end
 end
