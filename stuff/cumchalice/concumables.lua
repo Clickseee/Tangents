e7ad514ce3872c40f7c3a31f79d9debe23eb87ad
Clicky- <84652396+Clickseee@users.noreply.github.com>
2025-08-08 18:05:07 +0700

    FUCKED FUCKED FUCKED, ALL OF IT EHFNPO{_CO)I(U*DWSN

--- stuff/cumchalice/concumables.lua
+++ stuff/cumchalice/concumables.lua
@@ -5,9 +5,9 @@ SMODS.Consumable {
     pos = { x = 0, y = 0 },
     cost = 5,
     loc_txt = {
-        name = "{f:tngt_DETERMINATION}Susie's Idea",
+        name = "#",
         text = {
-            "{tngt_S:soul} {}{tngt_S:susie} {}{f:tngt_DETERMINATION}Susie's Idea"
+            "{tngt_S:soul,s:2} {}{tngt_S:susie,s:2} {}{f:tngt_DETERMINATION}Susie's Idea"
         }
     },
     use = function(self, card, area, copier)
@@ -16,58 +16,12 @@ SMODS.Consumable {
             trigger = 'after',
             delay = 0.4,
             func = function()
-                play_sound('tarot1', 1.1)
+                end_round()
+                play_sound('tngt_neverforget', 1.1)
                 used_tarot:juice_up(0.3, 0.5)
                 return true
             end
         }))
-        for i = 1, #G.hand.cards do
-            local percent = 1.15 - (i - 0.999) / (#G.hand.cards - 0.998) * 0.3
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.15,
-                func = function()
-                    G.hand.cards[i]:flip()
-                    play_sound('card1', percent)
-                    G.hand.cards[i]:juice_up(0.3, 0.3)
-                    return true
-                end
-            }))
-        end
-        for i = 1, #G.hand.cards do
-            G.E_MANAGER:add_event(Event({
-                func = function()
-                    local _card = G.hand.cards[i]
-                    if pseudorandom('royal_decree_'..i) < 0.5 then
-                        assert(SMODS.change_base(_card, nil, "Queen"))
-                        card:start_dissolve(nil, true)
-                        play_sound('tngt_neverforget', 1.2)
-                        return {}
-                        end
-                    if pseudorandom('royal_decree_'..i) < 0.5 then
-                        assert(SMODS.change_base(_card, nil, "King"))
-                        _card:set_ability('m_steel')
-                        play_sound('tarot1', 1.1, 0.4)
-                    end
-                    _card.states.visible = true
-                    return true
-                end
-            }))
-        end
-        for i = 1, #G.hand.cards do
-            local percent = 0.85 + (i - 0.999) / (#G.hand.cards - 0.998) * 0.3
-            G.E_MANAGER:add_event(Event({
-                trigger = 'after',
-                delay = 0.15,
-                func = function()
-                    G.hand.cards[i]:flip()
-                    G.hand.cards[i]:juice_up(0.3, 0.3)
-                    return true
-                end
-            }))
-        end
-
-        delay(0.5)
     end,
     can_use = function(self, card)
         return G.hand and #G.hand.cards > 0
